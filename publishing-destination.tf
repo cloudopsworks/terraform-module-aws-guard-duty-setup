391c8f46dab940de29ceb53092abdf9abc72342d
Cristian Beraha <cristian@cloudops.works>
2025-06-17 12:53:14 -0300

    fix: Add missing trailing comma in publishing destination access policy
    Included a trailing comma in the `identifiers` block of the publishing destination access policy for improved syntax and consistency. This resolves formatting issues that could lead to errors during Terraform plan and apply operations.

--- publishing-destination.tf
+++ publishing-destination.tf
@@ -119,7 +119,7 @@ data "aws_iam_policy_document" "publishing_destination_kms_key_policy" {
     principals {
       type = "AWS"
       identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${try(var.settings.publishing_destination.kms_key_admin_role, "terraform-access-role")}"
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${try(var.settings.publishing_destination.kms_key_admin_role, "terraform-access-role")}",
         "arm:aws:iam::${data.aws_caller_identity.current.account_id}:root"
       ]
     }
